8bf06ebf8e23a21fa1f69ce90af9f6924c8afd1e
MatiasPViecho <mpviecho@gmail.com>
2024-03-03 01:52:16 -0300

    refactor: change Cards section to be server side

--- sections/Cards.tsx
+++ sections/Cards.tsx
@@ -1,62 +1,31 @@
-'use client';
-const API_URL: string = process.env.NEXT_PUBLIC_API_URL || '';
 import { getPokemons } from '@/app/actions';
 import Card from '@/components/Card';
 import { Draggable } from '@/helpers/Draggable';
 import { IFinalPokemon, IError } from '@/interfaces/interfaces';
-import { useState, useEffect } from 'react';
 export const Cards = () => {
-  console.log(
-    '`${API_URL}/pokemon?offset=0&limit=151`:',
-    `${API_URL}/pokemon?offset=0&limit=151`
-  );
-  const [cards, setCards] = useState<IFinalPokemon[]>([]);
-  const [error, setError] = useState<string | null>();
-  const [isGrabbing, setIsGrabbing] = useState<boolean>(false);
-  useEffect(() => {
-    setError(null);
-    getPokemons().then((e: IFinalPokemon[] | IError) => {
-      console.log('e:', e);
-      if (typeof e == 'undefined') {
-        setError('Error general');
-        return;
-      }
-      if (!('ok' in e)) {
-        setCards(e);
-      } else {
-        setError(e.message);
-      }
-    });
-  }, []);
-
-  return !error ? (
-    <Draggable
-      rootClass={''}
-      handleStartGrabbing={() => setIsGrabbing(true)}
-      handleStopGrabbing={() => setIsGrabbing(false)}
-    >
+  return getPokemons().then((e: IFinalPokemon[] | IError) => (
+    <Draggable rootClass={''}>
       <ul
-        className={`flex p-8 gap-4 *:select-none overflow-x-scroll w-full min-w-[768px] ${
-          isGrabbing ? 'cursor-grabbing' : 'cursor-grab'
-        }`}
+        className={`flex p-8 gap-4 *:select-none overflow-x-scroll w-full min-w-[768px]
+         active:cursor-grabbing  cursor-grab
+        `}
       >
-        {cards.map((card) => (
-          <li
-            key={card.id}
-            className="snap-start snap-always flex-shrink-0"
-          >
-            <Card
-              sprite={card.sprite}
-              stats={card.stats}
-              name={card.name}
-              id={card.id}
-              type={card.type}
-            />
-          </li>
-        ))}
+        {Array.isArray(e) &&
+          e.map((card: IFinalPokemon) => (
+            <li
+              key={card.id}
+              className="snap-start snap-always flex-shrink-0"
+            >
+              <Card
+                sprite={card.sprite}
+                stats={card.stats}
+                name={card.name}
+                id={card.id}
+                type={card.type}
+              />
+            </li>
+          ))}
       </ul>
     </Draggable>
-  ) : (
-    <div>Ocurrió un error</div>
-  );
+  ));
 };
