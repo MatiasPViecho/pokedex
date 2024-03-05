4dc4a72d69f05ee284b6ac71cae6d515e5b628b3
MatiasPViecho <mpviecho@gmail.com>
2024-03-05 02:06:51 -0300

    feat: initial flavor, style changes

--- components/Card.tsx
+++ components/Card.tsx
@@ -39,7 +39,7 @@ const Card = ({
   ];
   return (
     <div
-      className={`px-1 pb-8 backdrop-blur-xl min-h-96 ml-4 min-w-80 rounded-lg bg-gradient-to-br from-${type} via-65% to-90% ${
+      className={`pb-8 backdrop-blur-xl min-h-96 min-w-80 rounded-lg bg-gradient-to-br from-${type} via-65% to-90% ${
         types.includes(type)
           ? `${
               second_type
@@ -56,31 +56,33 @@ const Card = ({
         name={name}
         type={types.includes(type) ? type : null}
       />
-      <div className="flex flex-col">
-        <div
-          className={`flex w-full justify-center p-4
+      <div
+        className={`flex w-full justify-center p-4
          ${
            types.includes(type) ? `bg-${type}` : 'bg-white'
          } rounded-b-md relative overflow-hidden border-4 border-gray-700/30`}
-        >
-          <img
-            className="w-2/3 h-auto select-none
+      >
+        <img
+          className="w-2/3 h-auto select-none
           "
-            src={sprite}
-            alt={`${name} pokemon sprite`}
-          />
-          <div className="absolute w-full h-full top-0 bg-gradient-to-b  from-holo-green to-holo-pink to-holo-purple to-holo-blue bg-cover animate-holo blur-xl opacity-35 "></div>
-        </div>
-        <div className="w-11/12 mx-auto text-sm gap-4 flex justify-center p-1 border-gray-800 bg-gray-700 border-x-4 border-y-2 rounded-full rounded-tr-[3500px] rounded-bl-[3500px] -mt-8 z-50 relative before:absolute before:content-[''] before:h-full before:w-2 before:top-0 before:bg-gray-200/30 before:animate-shine overflow-hidden before:-left-16">
-          <span>NO. {formatNumber(id)}</span>
-          <span>HT: {height}&quot;</span>
-          <span>WT: {weight} lbs</span>
-        </div>
-        <div
-          className={`mx-auto mt-4 max-w-[38ch] text-[0.75rem] font-semibold text-black`}
-        >
-          {<p>{flavor_text.es}</p> || <p>{flavor_text.en}</p>}
-          {/*   <ul className="grid grid-cols-2 gap-2 *:grid *:justify-end *:items-center *:gap-4 *:grid-cols-[16px_minmax(0px,_2fr)_24px] *:max-w-56">
+          src={sprite}
+          alt={`${name} pokemon sprite`}
+        />
+        <div className="absolute w-full h-full top-0 bg-gradient-to-b  from-holo-green to-holo-pink to-holo-purple to-holo-blue bg-cover animate-holo blur-xl opacity-35 "></div>
+      </div>
+      <div className="w-11/12 mx-auto text-sm gap-4 flex justify-center p-1 border-gray-800 bg-gray-700 border-x-4 border-y-2 rounded-full rounded-tr-[3500px] rounded-bl-[3500px] -mt-8 z-50 relative before:absolute before:content-[''] before:h-full before:w-2 before:top-0 before:bg-gray-200/30 before:animate-shine overflow-hidden before:-left-16">
+        <span>NO. {formatNumber(id)}</span>
+        <span>HT: {height}&quot;</span>
+        <span>WT: {weight} lbs</span>
+      </div>
+      <div
+        className={`px-2 w-full pt-4  bg-gradient-to-br from-white/30 from-0% via-white/25 via-10% to-transparent h-full to-40%  `}
+      >
+        <span className="max-w-[38ch] whitespace-break-spaces tracking-wide text-black italic text-balance text-[0.75rem]">
+          {flavor_text.es || flavor_text.en}
+        </span>
+
+        {/*   <ul className="grid grid-cols-2 gap-2 *:grid *:justify-end *:items-center *:gap-4 *:grid-cols-[16px_minmax(0px,_2fr)_24px] *:max-w-56">
             <li>
               {Icon.Sword}
               <span className="col-start-2">Attack: </span>
@@ -110,7 +112,6 @@ const Card = ({
               <span className="col-start-3">{stats.speed}</span>
             </li>
           </ul>*/}
-        </div>
       </div>
     </div>
   );
