a07afbe84aa35dd6879557b2d91f92e893b36fbc
MatiasPViecho <mpviecho@gmail.com>
2024-03-07 00:16:05 -0300

    chore: remove log

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -2,7 +2,6 @@ import Cards from '@/sections/Cards';
 
 export default function RegionId({ params }: { params: { id: number } }) {
   const isNumber = Number(params.id);
-  console.log('isNumber:', isNumber);
   return !isNaN(isNumber) ? (
     <Cards id={params.id} />
   ) : (
