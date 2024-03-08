e6701f3b78f0a7433af69fcfb02b4b9aeb17e6b6
MatiasPViecho <mpviecho@gmail.com>
2024-03-08 04:46:36 -0300

    feat: add zustand to manage context states

--- components/FilterMenu.tsx
+++ components/FilterMenu.tsx
@@ -1,9 +1,35 @@
+import { ChangeEvent } from 'react';
 import { MenuUl } from './Menu';
+import { create } from 'zustand';
+
+interface FilterState {
+  name: string;
+  change: (by: string) => void;
+}
+export const useFilterStore = create<FilterState>((set) => ({
+  name: '',
+  change: (by) => set((state) => ({ name: by })),
+}));
 export const FilterMenu = () => {
+  const { name, change } = useFilterStore();
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    if (name.length > 20 && name.length < value.length) {
+      return;
+    }
+    if (e.target.value.length > 21) {
+      change(e.target.value.slice(0, 20));
+    } else {
+      change(e.target.value);
+    }
+  };
   return (
     <MenuUl classes="bg-white w-full rounded-sm">
-      <li>test</li>
-      <li>test 2</li>
+      <input
+        value={name}
+        onChange={(e) => handleInputChange(e)}
+      />
+      {name}
     </MenuUl>
   );
 };
